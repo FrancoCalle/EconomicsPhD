2a4afe16a46253b67d60de4a168a6fdddf26dad6
Franco Calle <francocalle93@gmail.com>
2022-02-15 11:14:54 -0600

    computed expected number of firms entering and expected entry by firm

--- Second_Year/industrial_organization_2/ps2/ps2_q2.jl
+++ Second_Year/industrial_organization_2/ps2/ps2_q2.jl
@@ -25,14 +25,25 @@ end
 struct ModelData
     T::Int #Number of Markets
     K::Int #Number of Potential Entrants
-    X::Array #Number of Markets
-    Z::Array #Number of Potential Entrants
-    Firm::Array # Firm-market specific characteristic
-    Market::Array # Elasticity
+    X::Array #Market level characteristic
+    Z::Array #Firm-Market characteristic
+    Firm::Array # Firm index 
+    Market::Array # Market index
+end
+
+struct EstimationData
+    T::Int #Number of Markets
+    K::Int #Number of Potential Entrants
+    X::Array #Market level Characteristic
+    Z::Array #Firm-Market characteristic
+    I::Array #Indicator for entrants
+    N::Array #Number of Entrants
+    Firm::Array # Firm index
+    Market::Array # Market index
 end
 
 
-function heterogeneous_profitability(z_it, η_it, ϵ_it; mp)
+function heterogeneous_profitability(z_it, η_t, ϵ_it; mp)
 
     γ = mp.ggamma; β = mp.β; α = mp.aalpha; δ = mp.δ; ρ = mp.rho
 
@@ -42,11 +53,9 @@ function heterogeneous_profitability(z_it, η_it, ϵ_it; mp)
 end
 
 
-function profit_function(x_t, z_it, ϵ_it, η_t; mp)
+function profit_function(x_t, z_it, ϕ_it, n_t; mp)
     
     γ = mp.ggamma; β = mp.β; α = mp.aalpha; δ = mp.δ; ρ = mp.rho
-
-    ϕ_it = heterogeneous_profitability(z_it, ϵ_it, mp)
 
     v_t = γ + x_t * β - δ * log(n_t)
 
@@ -56,53 +65,109 @@ function profit_function(x_t, z_it, ϵ_it, η_t; mp)
 end
 
 
+function get_n_entrants(X, Z, Φ; mp , T, K)
+    
+    Π = zeros(K, T)
+    
+    for tt in 1:T
+
+        n_t = 0
+        
+        x_t = X[tt] # Market level characteristic
+                
+        profitability_index = sortperm(Φ[:,tt], rev=true)
+
+        for kk in 1:K
+            
+            nn = profitability_index[kk]
+
+            z_it = Z[nn, tt]
+
+            ϕ_it = Φ[nn, tt]
+
+            Π[nn, tt] = profit_function(x_t, z_it, ϕ_it, n_t; mp)        
+
+            n_t += 1 
+
+        end
+    end
+
+    I = Π.>0
+    
+    N = sum(I, dims=1)
+
+    return Π, I, N
+
+end
+
+
 function generate_nash_equilibrium(md, mp)
 
     # Unpack Data:
-    X = md.X; Z = md.Z; Firms = md.Firm; Market = md.Market; T = md.T; K = md.K
+    X = md.X; Firms = md.Firm; Market = md.Market; T = md.T; K = md.K; Z = md.Z
     # Unpack Parameters:
     γ = mp.ggamma; β = mp.β; α = mp.aalpha; δ = mp.δ; ρ = mp.rho
 
     # Draw market specific shocks:
     Eta = rand(Normal(0,1), T)
-    Epsilon = rand(Normal(0,1), T*K)
+    Epsilon = rand(Normal(0,1), T, K)
 
-    Φ = heterogeneous_profitability.(Z, Eta[Market], Epsilon; mp)
-    Π = zeros(T*K)
+    Φ = heterogeneous_profitability.(Z[:], Eta[Market], Epsilon[:]; mp)
+    Φ = reshape(Φ, K, T)
 
-    for jj in 1:T*K
+    Π, I, N = get_n_entrants(X, Z, Φ; mp, T, K)
 
-        tt = Market[jj]
-
-        x_t = X[tt]
-        η_t = Eta[tt]
-
-        z_it = Z[jj]
-        ϵ_it = Epsilon[jj]
-
-        Π[jj] = profit_function(x_t, z_it, ϵ_it, η_t; mp)        
-
-    end
-
-
-    return n_star
+    return Π, I, N
 
 end
-
 
 
 mp = ModelParameters(1,2,6,3,0.8)
 
 T = 10; K = 30;
-X = exp.(rand(Normal(0,1), K));
-Z = rand(Normal(0,1), K * T);
+X = exp.(rand(Normal(0,1), T));
+Z = rand(Normal(0,1), K , T);
 Firm = vcat([collect(1:30) for ii = 1:T]...);
 Market = vcat([Int.(ones(K)*ii) for ii = 1:T]...);
 
 
 md = ModelData(T, K, X, Z, Firm, Market);
+Π, I, N = generate_nash_equilibrium(md, mp)
+
+estimationData = EstimationData(T, K, X, Z, I, N, Firm, Market)
 
 
+function model_prediction(estimationData, mp; S=20)
 
+    # Unpack Data:
+    T = estimationData.T; 
+    K = estimationData.K; 
+    X = estimationData.X; 
+    Z = estimationData.Z;
+    I = estimationData.I;
+    N = estimationData.N';
+    Market= estimationData.Market
 
+    # Unpack Parameters:
+    γ = mp.ggamma; β = mp.β; α = mp.aalpha; δ = mp.δ; ρ = mp.rho
+
+    # S draws:
+    Eta_S = [rand(Normal(0,1), T, S) for ss = 1:S];
+    Epsilon_S = [rand(Normal(0,1), T, K) for ss in 1:S];
+
+    # S Simulations
+    I_expected = zeros(K,T)
+    N_expected = zeros(T)
+
+    for ss = 1:S
+        Φ = heterogeneous_profitability.(Z[:], Eta_S[ss][Market], Epsilon_S[ss][:]; mp)
+        Φ = reshape(Φ, K, T)
+        _, I_s, N_s = get_n_entrants(X, Z, Φ; mp, T, K)
+        I_expected .+= I_s./S
+        N_expected .+= N_s[:]./S
+    end
+
+    return I_expected, N_expected
+    
+end
 
